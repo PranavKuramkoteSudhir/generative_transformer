Fixed exploding gradients problem (During normalization epsilon was multiplied to the dinominator instead of being added) added model chechpoint functions

src/generate.py
@@ -28,8 +28,9 @@ def text_to_token_ids(text, tokenizer=tiktoken.get_encoding('gpt2')):
 def generate_text(model,idx,max_new_tokens,context_size,temperature=0,top_k=None,eos_id=None):
     logging.info('running generate_text')
     try:
-        for _ in range(max_new_tokens):
-            context=idx[:,-context_size:]
+        context=idx[:,-context_size:]
+        for i in range(max_new_tokens):
+            context=context[:,-context_size:]
             with torch.no_grad():
                 logits=model(context)
                 logits=logits[:,-1,:]
@@ -45,7 +46,9 @@ def generate_text(model,idx,max_new_tokens,context_size,temperature=0,top_k=None
                 next_index=torch.argmax(logits,dim=-1,keepdim=True)
             if eos_id==next_index:
                 break
+            logging.info(f"Generated Token: {next_index.item()} -> {token_to_text(next_index)}")
             context=torch.cat([context,next_index],dim=1)
+            
         return context
     except Exception as e:
         logging.info('__Error Occoured__')

src/mainloop/forward_pass.py
@@ -12,6 +12,7 @@ from src.data_prep.tokenization import CustomDataset
 from src.data_prep.data_load import CustomDataLoad
 from src.utils import get_text
 from src.generate import generate_text,token_to_text,text_to_token_ids
+from src.checkpoint import save_model
 
 def calc_loss(input,target,model,device):
     logging.info('running calc_loss')
@@ -130,12 +131,13 @@ if __name__=='__main__':
     model=GPTModel()
     model.to(device)
     
-    optimizer=torch.optim.AdamW(model.parameters(),lr=0.0004,weight_decay=0.01)
+    optimizer=torch.optim.AdamW(model.parameters(),lr=5e-4,weight_decay=0.01)
     epochs=5
 
     training_loop(model,data_load_train,data_load_test,optimizer,device,epochs,5,evaluation_iteration=5)
-    starter="In a few days Mr. Bingley returned Mr. Bennet’s visit, and sat about ten minutes with him in his library. He had entertained hopes of being admitted to a sight of the young ladies, of whose beauty he had heard much; but he saw only the father. The ladies were somewhat more fortunate, for they had the advantage of ascertaining, from an upper window, that he wore a blue coat and rode a black horse."
-    generated_tokens=generate_text(model,text_to_token_ids(text=starter),25,int(os.getenv('context_len')),1.4,35,None)
+    save_model(model,optimizer,'artifacts/models/model_and_optimizer.pth')
+    starter="This came as a surprize to alice she couldn't believe her eyes she didn't understand what was going one she was shocked to say the least"
+    generated_tokens=generate_text(model,text_to_token_ids(text=starter),100,int(os.getenv('context_len')),1.4,500,None)
     generated_text=token_to_text(token_ids=generated_tokens)
     logging.info(generated_text)
 

src/transformer/components/multiehaded_attn.py
@@ -2,9 +2,10 @@ from src.logger import logging
 from src.exceptions import CustomException
 import torch
 import sys
-
+# Everything seems to be working as expected
 class Multihead_attn(torch.nn.Module):
     def __init__(self,dIn,dOut,attnHeads,context,dropout=0.1,qkv_bias=False):
+        logging.info('Init Multihead_attn')
         try:
             super().__init__()
             if dOut%attnHeads!=0:
@@ -73,7 +74,9 @@ if __name__=='__main__':
 
     batch_size, context_length, d_in = batch.shape
     d_out = 6
-    mha = Multihead_attn(d_in, d_out,2,context_length, 0.0,False )
+    mha = Multihead_attn(d_in, d_out,2,context_length, 0.1,False )
     context_vecs = mha(batch)
     print(context_vecs)
     print("context_vecs.shape:", context_vecs.shape)
+    context_vecs.mean().backward()
+    print(mha.query.weight.grad)  # Check if gradients are non-None

src/transformer/components/transformer_components.py
@@ -21,7 +21,7 @@ class Normalize(torch.nn.Module):
         try:
             mean=x.mean(dim=-1,keepdim=True)
             var=x.var(dim=-1,keepdim=True,unbiased=False)
-            x_norm=(x-mean)/torch.sqrt(var*self.eps)
+            x_norm=(x-mean)/torch.sqrt(var+self.eps)
             x_norm=(x_norm*self.scale) +self.shift
             return x_norm
         except Exception as e: